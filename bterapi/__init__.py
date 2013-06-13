854fca69130c5193341ae5f3c90caea8284de6ee
hsharrison <henry.schafer.harrison@gmail.com>
2013-06-13 11:21:56 -0400

    Small change in __init__

--- bterapi/__init__.py
+++ bterapi/__init__.py
@@ -3,5 +3,5 @@
 from public import getDepth, getTradeHistory
 from trade import TradeAPI
 from keyhandler import KeyHandler
-from common import all_currencies, all_pairs, max_digits, formatCurrency, formatCurrencyDigits, \
+from common import all_currencies, all_pairs, max_digits, formatCurrency, fees, formatCurrencyDigits, \
     truncateAmount, truncateAmountDigits, BTERConnection
