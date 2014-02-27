72c35dbc5951775180bf11ea52f9b5447d15bc6b
Matt Walker <matt.g.d.walker@gmail.com>
2014-02-27 15:17:39 -0500

    Updated API, refactored netcode, and added ticker support.
    In common.py:
      * Factored out http-request code from `common.py' into `bterconnection.py'.
        ** Preserved backwards compatibility by importing moved symbols
           directly into the `common' namespace.
      * `all_pairs' list is now created at runtime via a request to data.bter.com.
    
    In bterconnection.py:
      * Moved `domain' variable into `BTERConnection' class; it is only
        used by this class.
        ** Changed it from `bter.com' to `data.bter.com' to reflect changes to api.
    
    In ticker.py
      * Added `Ticker' class to request ticker data from data.bter.com.

---
+++ bterapi/bterconnection.py
@@ -0,0 +1,37 @@
+import httplib
+import json
+import decimal
+
+class BTERConnection:
+    domain = 'data.bter.com'
+    def __init__(self, timeout=30):
+        self.conn = httplib.HTTPSConnection(self.domain, timeout=timeout)
+
+    def close(self):
+        self.conn.close()
+
+    def makeRequest(self, url, method='POST', extra_headers=None, params=''):
+        headers = {"Content-type": "application/x-www-form-urlencoded"}
+        if extra_headers is not None:
+            headers.update(extra_headers)
+
+        self.conn.request(method, url, params, headers)
+        response = self.conn.getresponse().read()
+
+        return response
+
+    def makeJSONRequest(self, url, method='POST', extra_headers=None, params=""):
+        response = self.makeRequest(url, method, extra_headers, params)
+        return parseJSONResponse(response)
+
+def parseJSONResponse(response):
+    def parse_decimal(var):
+        return decimal.Decimal(var)
+
+    try:
+        r = json.loads(response, parse_float=parse_decimal, parse_int=parse_decimal)
+    except Exception as e:
+        msg = "Error while attempting to parse JSON response: %s\nResponse:\n%r" % (e, response)
+        raise Exception(msg)
+
+    return r
