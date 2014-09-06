65cb7559172cab1378ab448aca135f92b83e5a75
Konstantinos Vaggelakos <kozze89@gmail.com>
2014-09-06 15:53:24 -0700

    __init__ imports update

--- bterapi/__init__.py
+++ bterapi/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2013 Alan McIntyre
 
-from public import getDepth, getTradeHistory
-from trade import TradeAPI
-from keyhandler import KeyHandler
-from common import all_currencies, all_pairs, max_digits, formatCurrency, fees, formatCurrencyDigits, \
+from bterapi.public import getDepth, getTradeHistory
+from bterapi.trade import TradeAPI
+from bterapi.keyhandler import KeyHandler
+from bterapi.common import all_currencies, all_pairs, max_digits, formatCurrency, fees, formatCurrencyDigits, \
     truncateAmount, truncateAmountDigits, BTERConnection
