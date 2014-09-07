3b1f71027e7fa07bd7e48241bdcc8c241fada82a
Konstantinos Vaggelakos <kozze89@gmail.com>
2014-09-07 16:29:30 -0700

    Removing use of zip in getDepth

--- bterapi/public.py
+++ bterapi/public.py
@@ -19,28 +19,16 @@ def getDepth(pair, connection=None, error_handler=None):
     depth = common.validateResponse(connection.makeJSONRequest('/api/1/depth/%s' % pair, method='GET'),
                                     error_handler=error_handler)
 
-    asks = depth.get(u'asks')
+    if type(depth) is not dict:
+        raise TypeError("The response is not a dict.")
+
+    asks = depth.get('asks')
     if type(asks) is not list:
-        raise Exception("The response does not contain an asks list.")
+        raise TypeError("The response does not contain an asks list.")
 
-    bids = depth.get(u'bids')
+    bids = depth.get('bids')
     if type(bids) is not list:
-        raise Exception("The response does not contain a bids list.")
-
-    if len(asks) > 0:
-        ask_prices, ask_sizes = zip(*asks)
-        ask_prices = [decimal.Decimal(p) for p in ask_prices]
-        ask_sizes = [decimal.Decimal(s) for s in ask_sizes]
-        asks = zip(ask_prices, ask_sizes)
-    else:
-        asks = []
-    if len(bids) > 0:
-        bid_prices, bid_sizes = zip(*bids)
-        bid_prices = [decimal.Decimal(p) for p in bid_prices]
-        bid_sizes = [decimal.Decimal(s) for s in bid_sizes]
-        bids = zip(bid_prices, bid_sizes)
-    else:
-        bids = []
+        raise TypeError("The response does not contain a bids list.")
 
     return asks, bids
 
